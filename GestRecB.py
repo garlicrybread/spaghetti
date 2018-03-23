16280706aac38b5140125909b432ac832bff7665
Ember <garlicrybread@gmail.com>
2018-03-23 08:44:56 -0400

    added main file, colorRec, and updated GestRec

--- GestRecB.py
+++ GestRecB.py
@@ -2,18 +2,21 @@
 
 import json
 import os
+import time
 
 
 class GestureRecognition:
     def __init__(self):
         # for debugging purposes
-        print os.system("pwd")
         self.openposeDir = "build/examples/openpose/openpose.bin"
 
-    def execute(self, input_file_dir, output_file_dir):
+    def execute(self, input_file_dir, output_file_dir,headDisplay):
         # must be in openpose to be able to run openpose
         os.chdir("/home/team18/openpose")
         os.system("streamer -f jpeg -o " + input_file_dir + "/webcam.jpeg -c /dev/video1")
+        # time.sleep(0.5)
+        os.system("streamer -f jpeg -o " + input_file_dir + "/webcam.jpeg -c /dev/video1")
+        headDisplay.display_image("/home/team18/Grasp-Detector-master/sawyer_head/got_it.JPG")
         os.system(
             self.openposeDir + " --image_dir " + input_file_dir + " --write_keypoint_json " + output_file_dir +
             " --write_images " + output_file_dir + " --no_display")
@@ -33,10 +36,11 @@ class GestureRecognition:
         # Find which absolute distance is greater --> this will be the actual
         # direction that the user is pointing
         if abs(DirL) <= abs(DirR):
-            # pointing right is given value of 0
+            # pointing right with respect to the user is given value of 0
             this_way = 0
         else:
-            # pointing left is given value of 1, defaults to pointing left
+            # pointing left with respect to the user is given value of 1, defaults to pointing left
             this_way = 1
 
+        os.chdir("/home")
         return this_way
